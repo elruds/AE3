3899ccef2cb214c5724e0577827cabcd8007924e
elruds <32840432+elruds@users.noreply.github.com>
2017-12-31 14:03:55 +0000

    Add files via upload

--- AssEx3.java
+++ AssEx3.java
@@ -1,3 +1,24 @@
+/** This program retrieves information about fitness classes (time of class, class name and 
+ * tutor name) from a file ("ClassesIn.txt") and displays it as a timetable in a GUI.  If there is no class 
+ * scheduled at a particular time, the time slot is shown as "Available".  The user can schedule
+ * a class in an "Available" time slot by entering class time, class name and tutor name in
+ * the text fields and clicking "Add".  Alternatively they can remove a scheduled class by 
+ * entering the same information and clicking "Delete".
+ * 
+ *
+ * The program retrieves attendance figures for these classes from a different file("AttendancesIn.txt").  By clicking "View
+ * attendances" the user is able to view an attendance report which is displayed in a separate
+ * window.  This contains a table showing the fitness class ID, class name, tutor name, 
+ * attendance figures for five weeks, average attendance for each class over the five weeks 
+ * and overall average attendance over the five weeks.
+ * 
+ * When the user clicks "Save and Exit", a file called "ClassesOut.txt" is created which contains 
+ * information (class ID, class name, tutor name and class start time) about the classes in the
+ * timetable.
+ * 
+ * 
+ * */
+ 
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -16,47 +37,12 @@ public class AssEx3 {
 	 */
 
 	public static void main(String[] args) {
-		SportsCentreGUI display = new SportsCentreGUI();
+		SportsCentreGUI display = new SportsCentreGUI(); 
 		display.setVisible(true);
+		
 	}
 }
-//		
-//		FileReader reader = null;
-//		try	{
-//			try {
-//			reader = new FileReader("/Users/Lauren/eclipse-workspace/AssEx3/ClassesIn.txt");
-//			Scanner scan = new Scanner(reader);
-//			FitnessClass fc;
-//			FitnessProgram fp = new FitnessProgram();
-//			
-//			while (scan.hasNextLine() ) {
-//				String classLine = scan.nextLine();
-//				fc = new FitnessClass(classLine);
-//				fp.addFitnessClass(fc);
-//				
-//			}
-//			
-//			System.out.print(fp.getFirstAvailableClassTime());
-//			}
-//			finally {
-//
-//				if (reader != null) {
-//					reader.close();
-//					}
-//				}
-//			}
-//		
-//			catch (IOException ioe) {
-//				JOptionPane.showMessageDialog(null, "File not found",
-//						"Error", JOptionPane.ERROR_MESSAGE);
-//
-//			}
-//			catch (InputMismatchException e) {
-//				JOptionPane.showMessageDialog(null, "Invalid file content",
-//						"Error", JOptionPane.ERROR_MESSAGE);
-//			}
-//		}
-//	}
+
 
 
 
